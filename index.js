c9df493640a67a3ca2e5208ce17732d99364916d
Amanpati1712 <amanpati1712@gmail.com>
2024-03-14 14:33:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("chai or code")
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/login', (req, res) => {
+    res.send('Hello World! login world')
+  })
+
+  app.get('/youtube', (req, res) => {
+    res.send('Hello World! youtube world')
+  })
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
